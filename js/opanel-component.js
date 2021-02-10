572ef98cc9fab18388ca51f5186c4c6d20b47f76
Sikaar <sikaar@gmail.com>
2021-02-10 16:26:39 +0100

    adding the fit texture component
    allowing to automatically resize the geometry of plane with logos

--- js/opanel-component.js
+++ js/opanel-component.js
@@ -69,3 +69,64 @@ AFRAME.registerComponent('thumbstick-logging',{
   }
 });
 
+
+AFRAME.registerComponent('fit-texture', {
+  dependencies: ['geometry', 'material'],
+  schema: {
+    type: 'boolean',
+    default: true
+  },
+   update: function () {
+     if (this.data === false) return;
+
+     var el = this.el;
+     var self = this;
+     if (self.dimensions) {
+       // If texture has already been loaded, and `fit-texture` was reset.
+       self.applyTransformation();
+     } else {
+       var textureLoaded = function(e) {
+
+          var w = e.detail.texture.image.videoWidth || e.detail.texture.image.width;
+          
+          var h = e.detail.texture.image.videoHeight || e.detail.texture.image.height;
+          
+          // Don't apply transformation on incomplete info
+          if(h === 0 || w === 0) return;
+          
+          // Save dimensions for later updates to `fit-texture`, see above.
+          self.dimensions = {w:w, h:h};
+          
+          self.applyTransformation();
+       }
+       //el.addEventListener('materialvideoloadeddata', textureLoaded);
+       el.addEventListener('materialtextureloaded', textureLoaded);
+      
+     }
+   },
+   
+   applyTransformation: function () {
+    var el = this.el;
+    var geometry = el.getAttribute('geometry');
+
+    // Use self.dimension data from previous texture/video loaded events
+    var widthHeightRatio = this.dimensions.h / this.dimensions.w;
+
+    if (geometry.width && geometry.height) {
+      console.warn('Using `fit-texture` component on an element with both width and height. Therefore keeping width and changing height to fit the texture. If you want to manually set both width and height, set `fit-texture="false"`. ');
+    }
+    if (geometry.width) {
+      el.setAttribute('height', geometry.width * widthHeightRatio);
+    } else if (geometry.height) {
+      el.setAttribute('width', geometry.height / widthHeightRatio);
+    } else {
+      // Neither width nor height is set.
+      var tempWidth = 1.0;
+      el.setAttribute('width', '' + tempWidth);
+      el.setAttribute('height', tempWidth * widthHeightRatio);
+    }
+  }
+
+});
+
+
